4de9dcb7f1b87de9c729007eae0fb5ae33ee08f3
gith <A@DESKTOP-DTAJTL4>
2021-03-18 17:57:18 +0900

    3/18 dbcp/jdbc/mybatis 설정 완료

--- src/main/java/com/cjc/seektam/HomeController.java
+++ src/main/java/com/cjc/seektam/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,6 @@ public class HomeController {
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
