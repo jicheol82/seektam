825ed5e9daa1ce7809554aa8f64b3b69bf6541f5
jane <jane@DESKTOP-F5TB2KN.mshome.net>
2021-03-20 21:39:30 +0900

    3/20 interceptor를 이용한 login 인증 적용

---
+++ src/main/java/com/cjc/seektam/interceptor/CertificationInterceptor.java
@@ -0,0 +1,41 @@
+package com.cjc.seektam.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CertificationInterceptor implements HandlerInterceptor{
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		String memId = (String) session.getAttribute("memId");
+		if(memId==null) {
+			response.sendRedirect("/seektam/login");
+			return false;
+		}else {
+			session.setMaxInactiveInterval(30*60);
+			return true;
+		}
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
